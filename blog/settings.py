8eeff18f190cc246e79ada9156d15ebf11731738
Chinyere Unamba <66808036+chinyereunamba@users.noreply.github.com>
2023-04-08 03:49:45 +0100

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ["mucco.herokuapp.com", '*']
+ALLOWED_HOSTS = ["mucco.herokuapp.com", '*', 'mucco.vercel.app]
 
 AUTH_USER_MODEL = "base.Account"
 
